3f1e225c6d7f24113f018f56b260b7eb71d0f2f4
Teterich Ilya <ilya.teterich@yandex.ru>
2022-01-23 17:50:20 +0300

    add eslint, configure ci (#1)

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function init() {
-  console.log("Welcome to the Brain Games!");
-  const username = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
 }
 
